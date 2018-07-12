8d9f17fb34cd694742f76c49bccb6215a9235cfc
Chung Hd <chunghd126@gmail.com>
2018-07-12 22:27:41 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:/usr/local/go/bin
 export GOROOT=/usr/local/go
-export GOPATH=/var/golang/politician_report
+export GOPATH=/var/golang/politician_report/vendor
 go build crawler.go
